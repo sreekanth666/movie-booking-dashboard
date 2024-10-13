5a80cffbe60abca8f5d625b0c1c264e594fe4435
sreekanth666 <93961521+sreekanth666@users.noreply.github.com>
2024-10-13 20:35:32 +0530

    feat: Add LoginPage component

---
+++ src/pages/login.tsx
@@ -0,0 +1,92 @@
+import { upperFirst, useToggle } from '@mantine/hooks';
+import React, { FC } from 'react';
+import { useForm } from '@mantine/form';
+import { Paper, Group, Stack, TextInput, PasswordInput, Checkbox, Anchor, Button, Text, PaperProps } from '@mantine/core';
+
+interface LoginPageProps extends PaperProps {
+
+}
+
+const LoginPage: FC<LoginPageProps> = ({ ...props }) => {
+    const [type, toggle] = useToggle(['login', 'register']);
+    const form = useForm({
+        initialValues: {
+            email: '',
+            name: '',
+            password: '',
+            terms: true,
+        },
+
+        validate: {
+            email: (val) => (/^\S+@\S+$/.test(val) ? null : 'Invalid email'),
+            password: (val) => (val.length <= 6 ? 'Password should include at least 6 characters' : null),
+        },
+    });
+
+    return (
+        <React.Fragment>
+            <div className='flex h-dvh items-center justify-center'>
+                <Paper radius="md" p="xl" withBorder {...props}>
+                    <Text size="lg" fw={500}>
+                        Welcome to Movie Booking, Please {type}
+                    </Text>
+    
+                    <form onSubmit={form.onSubmit(() => { })}>
+                        <Stack>
+                            {type === 'register' && (
+                                <TextInput
+                                    label="Name"
+                                    placeholder="Your name"
+                                    value={form.values.name}
+                                    onChange={(event) => form.setFieldValue('name', event.currentTarget.value)}
+                                    radius="md"
+                                />
+                            )}
+    
+                            <TextInput
+                                required
+                                label="Email"
+                                placeholder="hello@mantine.dev"
+                                value={form.values.email}
+                                onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
+                                error={form.errors.email && 'Invalid email'}
+                                radius="md"
+                            />
+    
+                            <PasswordInput
+                                required
+                                label="Password"
+                                placeholder="Your password"
+                                value={form.values.password}
+                                onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
+                                error={form.errors.password && 'Password should include at least 6 characters'}
+                                radius="md"
+                            />
+    
+                            {type === 'register' && (
+                                <Checkbox
+                                    label="I accept terms and conditions"
+                                    checked={form.values.terms}
+                                    onChange={(event) => form.setFieldValue('terms', event.currentTarget.checked)}
+                                />
+                            )}
+                        </Stack>
+    
+                        <Group justify="space-between" mt="xl">
+                            <Anchor component="button" type="button" c="dimmed" onClick={() => toggle()} size="xs">
+                                {type === 'register'
+                                    ? 'Already have an account? Login'
+                                    : "Don't have an account? Register"}
+                            </Anchor>
+                            <Button type="submit" radius="xl">
+                                {upperFirst(type)}
+                            </Button>
+                        </Group>
+                    </form>
+                </Paper>
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default LoginPage;
