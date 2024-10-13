5d77ba67bdbcd749e405e134b5d27dad74361465
sreekanth666 <93961521+sreekanth666@users.noreply.github.com>
2024-10-13 20:35:19 +0530

    refactor: Remove unused CSS styles and update routing in App component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { MantineProvider } from '@mantine/core'
+import '@mantine/core/styles.css';
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MantineProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MantineProvider>
   </StrictMode>,
 )
