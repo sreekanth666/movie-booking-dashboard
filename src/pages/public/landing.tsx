6e3c842e299ce633a4cc33615ff4be8119642503
sreekanth666 <93961521+sreekanth666@users.noreply.github.com>
2024-10-21 15:39:55 +0530

    feat: Add movie carousel and trending movies section

--- src/pages/public/landing.tsx
+++ src/pages/public/landing.tsx
@@ -1,13 +1,86 @@
 import React, { FC } from 'react';
+import { Carousel } from '@mantine/carousel';
+import { Image } from '@mantine/core';
+import MovieCard from '../../components/card-2';
 
 interface LandingPageProps {
 
 }
 
 const LandingPage: FC<LandingPageProps> = ({ }) => {
+    const movies = [
+        {
+            id: '123',
+            title: 'A.R.M',
+            image: 'https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/arm-et00407124-1726144274.jpg',
+            rating: 9.5
+        },
+        {
+            id: '124',
+            title: 'A.R.M',
+            image: 'https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/arm-et00407124-1726144274.jpg',
+            rating: 9.5
+        },
+        {
+            id: '123',
+            title: 'A.R.M',
+            image: 'https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/arm-et00407124-1726144274.jpg',
+            rating: 9.5
+        },
+        {
+            id: '123',
+            title: 'A.R.M',
+            image: 'https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/arm-et00407124-1726144274.jpg',
+            rating: 9.5
+        },
+        {
+            id: '123',
+            title: 'A.R.M',
+            image: 'https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/arm-et00407124-1726144274.jpg',
+            rating: 9.5
+        },
+        {
+            id: '123',
+            title: 'A.R.M',
+            image: 'https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/arm-et00407124-1726144274.jpg',
+            rating: 9.5
+        },
+        {
+            id: '123',
+            title: 'A.R.M',
+            image: 'https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/arm-et00407124-1726144274.jpg',
+            rating: 9.5
+        },
+        {
+            id: '123',
+            title: 'A.R.M',
+            image: 'https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/arm-et00407124-1726144274.jpg',
+            rating: 9.5
+        },
+    ]
     return (
         <React.Fragment>
-            Landing Page
+            <Carousel withIndicators height={400} loop >
+                <Carousel.Slide>
+                    <Image src={'/images/banner-1.avif'} />
+                </Carousel.Slide>
+                <Carousel.Slide>
+                    <Image src={'/images/banner-2.avif'} />
+                </Carousel.Slide>
+            </Carousel>
+
+            <div className='p-3'>
+                <p className='font-semibold text-xl mb-2'>Trending now</p>
+                <div className='grid grid-cols-6 gap-10'>
+                    {
+                        movies.map(movie => (
+                            <div key={movie.id} className='col-span-1'>
+                                <MovieCard id={movie.id} title={movie.title} image={movie.image} rating={movie.rating} />
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
         </React.Fragment>
     );
 };
